dfa536ca550d84466f128939b3c029fc81d6aa9d
Drew Krehel <drewkrehel@gmail.com>
2025-09-07 01:33:41 +0000

    commented out the old

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -3,50 +3,50 @@ class DiceController < ApplicationController
     render({ :template => "dice_templates/homepage" })
   end
 
-  def roll_two_six
-    @rolls = []
+  # def roll_two_six
+  #   @rolls = []
 
-    2.times do
-      dice = rand(1..6)
-      @rolls.push(dice)
-    end
+  #   2.times do
+  #     dice = rand(1..6)
+  #     @rolls.push(dice)
+  #   end
 
-    render({ :template => "dice_templates/result_two_six" })
-  end
+  #   render({ :template => "dice_templates/result_two_six" })
+  # end
 
-  def roll_two_ten
-    @rolls = []
+  # def roll_two_ten
+  #   @rolls = []
 
-    2.times do
-      dice = rand(1..10)
-      @rolls.push(dice)
-    end
+  #   2.times do
+  #     dice = rand(1..10)
+  #     @rolls.push(dice)
+  #   end
 
-    render({ :template => "dice_templates/result_two_ten" })
-  end
+  #   render({ :template => "dice_templates/result_two_ten" })
+  # end
 
-  def roll_one_twenty
-    @rolls = []
+  # def roll_one_twenty
+  #   @rolls = []
 
-    1.times do
-      dice = rand(1..20)
-      @rolls.push(dice)
-    end
+  #   1.times do
+  #     dice = rand(1..20)
+  #     @rolls.push(dice)
+  #   end
 
-    render({ :template => "dice_templates/result_one_twenty" })
-  end
+  #   render({ :template => "dice_templates/result_one_twenty" })
+  # end
 
-  def roll_five_four
+  # def roll_five_four
     
-    @rolls = []
+  #   @rolls = []
 
-    5.times do
-      dice = rand(1..4)
-      @rolls.push(dice)
-    end
+  #   5.times do
+  #     dice = rand(1..4)
+  #     @rolls.push(dice)
+  #   end
 
-    render({ :template => "dice_templates/result_five_four" })
-  end
+  #   render({ :template => "dice_templates/result_five_four" })
+  # end
 
   def flex_roll
     @num_dice = params.fetch("number").to_i
